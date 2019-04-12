314291ef113ebed1afbc4a934a46783cd8645237
ThisSimon <c00208558@itcalow.ie>
2019-04-12 12:22:08 +0100

    updated bash script

--- buildOppSys.sh
+++ buildOppSys.sh
@@ -1,2 +1,9 @@
-# curl http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-04-09/2019-04-08-raspbian-stretch-lite.zip -O -J -L
-unzip 2019-04-08-raspbian-stretch-lite.zip
+#!/bin/bash
+#sudo apt-get update
+#sudo apt-get install -y curl
+#sudo apt-get install -y util-linux
+#sudo curl http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-04-09/2019-04-08-raspbian-stretch-lite.zip -O -J -L
+#unzip 2019-04-08-raspbian-stretch-lite.zip
+#sha256sum 2019-04-08-raspbian-stretch-lite.zip
+#REM tis is comment following cmd uses prefered browser
+xdg-open https://www.raspberrypi.org/downloads/raspbian/&
