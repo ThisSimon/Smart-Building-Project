5ae24d15305b6f4109f0880181be2c114ada68cd
ThisSimon <c00208558@itcalow.ie>
2019-04-16 15:52:17 +0100

    updated arduino install .sh

--- arduinoTools/install-arduino-toolsAndLibraries.sh
+++ arduinoTools/install-arduino-toolsAndLibraries.sh
@@ -7,20 +7,28 @@
 #sudo apt-get install xz-utils
 
 ##Set version required
-#arduinoVersion=arduino-1.8.9-linux64
-#sudo curl https://downloads.arduino.cc/$arduinoVersion.tar.xz -O -J -L
-#sha512sum $arduinoVersion.tar.xz > zippedtar-xz-SHA512.txt
+arduinoVersion=arduino-1.8.9-linux64
+arduinoFolder=arduino-1.8.9
+if [ ! -e "$arduinoVersion.tar.xz" ]; then
+    sudo curl https://downloads.arduino.cc/$arduinoVersion.tar.xz -O -J -L
+fi
 
+if [ ! -e "zippedtar-xz-SHA512.txt" ];then
+    sha512sum $arduinoVersion.tar.xz > zippedtar-xz-SHA512.txt
+fi
 ## The following command retrives a list of hashes for various distrobutions
-#sudo curl https://downloads.arduino.cc/arduino-1.8.9.sha512sum.txt > arduino-sha512sum.txt
-
+if [ ! -e "arduino-sha512sum.txt" ]; then
+    sudo curl https://downloads.arduino.cc/arduino-1.8.9.sha512sum.txt > arduino-sha512sum.txt
+fi
 ## The following command extracts the relevant hash for downloded distro 
-#grep $arduinoVersion.tar.xz arduino-sha512sum.txt > theSha512sum.txt
+if [ ! -e "theSha512sum.txt" ]; then 
+    grep $arduinoVersion.tar.xz arduino-sha512sum.txt > theSha512sum.txt
+fi
 
 x=$(cat zippedtar-xz-SHA512.txt)
 y=$(cat theSha512sum.txt)
 
-echo $x
+printf $x
 printf $y
 
 if [ "$x" != "$y" ];
@@ -29,35 +37,37 @@ then
 #    exit 0
 fi
 
-printf "Hashes are OK and unpacking\n"
-#tar -xvJf $arduinoVersion.tar.xz
-
+if [ ! -e "$arduinoVersion" ]; then
+    printf "Hashes are OK and unpacking\n"
+    tar -xvJf $arduinoVersion.tar.xz
+fi
 ## Now we need libraries to allow ESP8268 to comunicate
 ## info at https://github.com/knolleary/pubsubclient
-#sudo curl https://github.com/knolleary/pubsubclient/archive/master.zip -O -J -L
-#unzip pubsubclient-master.zip
-#mv pubsubclient-master pubsubclient
-
+if [ ! -e "pubsubclient-master.zip" ]; then
+    sudo curl https://github.com/knolleary/pubsubclient/archive/master.zip -O -J -L
+    unzip pubsubclient-master.zip
+    mv pubsubclient-master pubsubclient
+fi
 ## The teperature and humidity libries are required
 ## info at https://github.com/adafruit/DHT-sensor-library
-#sudo curl https://github.com/adafruit/DHT-sensor-library/archive/master.zip -O -J -L
-#unzip DHT-sensor-library-master.zip
-#mv DHT-sensor-library-master DHT
-
+if [ ! -e "DHT-sensor-library-master.zip" ]; then
+    sudo curl https://github.com/adafruit/DHT-sensor-library/archive/master.zip -O -J -L
+    unzip DHT-sensor-library-master.zip
+    mv DHT-sensor-library-master DHT
+fi
+rm *.zip
 thisDirectory=$PWD
-sudo mv arduino-1.8.9 /opt/
-cd/opt/arduino-1.8.9
-#sudo ./install.sh
+if [ ! -e "$arduinoFolder" ]; then
+    ## The following library removes errors when starting arduino
+    sudo apt-get install libcanberra-gtk-module
+    sudo mv $arduinoFolder /opt/
+    cd /opt/$arduinoFolder
+    #sudo ./install.sh
+    cd $thisDirectory
+fi
 
-cd $thisDirectory
-## The following library removes errors when starting arduino
-sudo apt-get install libcanberra-gtk-module
-
-cp DHT ~/Arduino/libraries
-cp pubsubclient ~/Arduino/libraries
-<<<<<<< HEAD
-cp lightTempHumid ~/Arduino/libraries
-=======
+mv DHT ~/Arduino/libraries
+mv pubsubclient ~/Arduino/libraries
 cp lightTempHumid ~/Arduino/
 
 printf "Arduino should be installed now\n"
@@ -66,6 +76,4 @@ printf "Enter http://arduino.esp8266.com/stable/package_esp8266com_index.json\n"
 printf "into the Additional Board Manager URLs then press OK\n"
 printf "If problems with serial port permitions refer to following page\n"
 printf "https://www.arduino.cc/en/Guide/Linux\n" 
-printf "Start arduino then select file open then lightTempHumid should now be ready to complile\n"
-																				
->>>>>>> dc310f35a4b07e1e68182486eaad46be90bf91e3
+printf "Start arduino then select file open then lightTempHumid should now be ready to compile"
